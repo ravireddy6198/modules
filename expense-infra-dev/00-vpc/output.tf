09a207b9d3983d5d2a793af05d7b029b7a0c8a7e
ravireddy6198 <raviprakash03051992@gmail.com>
2025-02-17 08:54:15 +0530

    adding outputs to subnet ids

---
+++ expense-infra-dev/00-vpc/output.tf
@@ -0,0 +1,11 @@
+/* output "az_info" {
+  value = module.vpc.az_info
+}
+
+output "subnet_info" {
+  value = module.vpc.subnet_info
+} 
+
+output "default_vpc" {
+  value = module.vpc.default_vpc
+}*/
