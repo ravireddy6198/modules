f9a9b357283d0f1a42075491efb3c0815f8883be
ravireddy6198 <raviprakash03051992@gmail.com>
2025-02-17 12:48:18 +0530

    adding outputs to subnet ids

--- expense-infra-dev/10-sg/parameters.tf
+++ expense-infra-dev/10-sg/parameters.tf
@@ -1,5 +1,11 @@
 resource "aws_ssm_parameter" "mysql_sg_id" {
   name  = "/${var.project_name}/${var.environment}/mysql_sg_id"
   type  = "String"
-  value = module.mysql_sg.
+  value = module.mysql_sg.sg_id
+}
+
+resource "aws_ssm_parameter" "backend_sg_id" {
+  name  = "/${var.project_name}/${var.environment}/backend_sg_id"
+  type  = "String"
+  value = module.backend_sg.sg_id
 }
